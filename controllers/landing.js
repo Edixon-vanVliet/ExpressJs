908809b8fe97bab44cb460a844181b8fa0f202b9
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-06-29 01:40:14 -0400

    Add post handler

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,8 @@
 exports.getLanding = function (req, res, next) {
     res.render("landing", { title: "Express - Landing" });
 };
+
+exports.submitLead = function (req, res, next) {
+    console.log(`Lead email: ${req.body.leadEmail}`);
+    res.redirect("/");
+};
