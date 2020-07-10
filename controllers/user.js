f9f5d5967be7374dfb8ad8b29a165c2adaa6dae4
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-07-09 23:26:24 -0400

    Implement error handling for signup

--- controllers/user.js
+++ controllers/user.js
@@ -4,12 +4,19 @@ const passport = require("passport");
 const myPassport = require("../passport_setup")(passport);
 let flash = require("connect-flash");
 
+const { isEmpty } = require("lodash");
+const { validateUser } = require("../validators/signup");
+
 exports.showLogin = function (req, res, next) {
     res.render("user/login", { formData: {}, errors: {} });
 };
 
 exports.showSignup = function (req, res, next) {
     res.render("user/signup", { formData: {}, errors: {} });
+};
+
+const rerenderSignup = function (errors, req, res, next) {
+    res.render("user/signup", { formData: req.body, errors: errors });
 };
 
 exports.login = function (req, res, next) {
@@ -25,17 +32,24 @@ const generateHash = function (password) {
 };
 
 exports.signup = function (req, res, next) {
-    const newUser = models.User.build({
-        email: req.body.email,
-        password: generateHash(req.body.password),
-    });
+    let errors = {};
+    return validateUser(errors, req).then((errors) => {
+        if (!isEmpty(errors)) {
+            rerenderSignup(errors, req, res, next);
+        } else {
+            const newUser = models.User.build({
+                email: req.body.email,
+                password: generateHash(req.body.password),
+            });
 
-    return newUser.save().then((result) => {
-        passport.authenticate("local", {
-            successRedirect: "/",
-            failureRedirect: "/signup",
-            failureFlash: true,
-        })(req, res, next);
+            return newUser.save().then((result) => {
+                passport.authenticate("local", {
+                    successRedirect: "/",
+                    failureRedirect: "/signup",
+                    failureFlash: true,
+                })(req, res, next);
+            });
+        }
     });
 };
 
